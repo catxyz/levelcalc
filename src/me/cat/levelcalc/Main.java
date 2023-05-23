7baac17c618633978d24bde4cd7abe7d46a601d6
catxyz <69825595+catxyz@users.noreply.github.com>
2023-05-23 03:11:01 +0200

    Initial commit

---
+++ src/me/cat/levelcalc/Main.java
@@ -0,0 +1,89 @@
+package me.cat.levelcalc;
+
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.util.Scanner;
+
+public class Main {
+
+    private static final int EXP_CONST = 10_672_500;
+
+    private static int currentExpEarned;
+    private static int expLeft;
+    private static int expToSubtract;
+    private static int avgExpPerGame;
+    private static double daysSpan;
+
+    private static double avgGameTime;
+
+    public static void main(String[] args) {
+        getInfo();
+        printResults();
+    }
+
+    private static void getInfo() {
+        Scanner scanner = new Scanner(System.in);
+        newLine();
+
+        out("| Enter your current ExpEarned:");
+        currentExpEarned = scanner.nextInt();
+        expLeft = EXP_CONST - currentExpEarned;
+        newLine();
+
+        out("| Enter any exp you wish to subtract:");
+        expToSubtract = scanner.nextInt();
+        newLine();
+
+        out("| Enter avg. exp earned per game\n" + "  from the game you play:");
+        avgExpPerGame = scanner.nextInt();
+        newLine();
+
+        out("| Enter the time span for achieving this (days):");
+        daysSpan = scanner.nextDouble();
+        newLine();
+
+        out("| Enter avg. time it takes to complete a game (minutes):");
+        avgGameTime = scanner.nextDouble();
+        newLine();
+    }
+
+    private static void printResults() {
+        out("------------------------------------");
+        out("Games left to level 100       : " + format(calculateGamesLeft()));
+        out("Games required per day        : " + format(calculateGamesPerDay()));
+        out("Time it'll take daily (hours) : " + format(calculateDailyTimeRequirement()) + "h");
+        out("Percentage to level 100       : " + format(calculatePercentage()) + "%");
+        out("Exp left                      : " + format(expLeft));
+        out("------------------------------------");
+    }
+
+    public static double calculateGamesLeft() {
+        return ((double) expLeft - expToSubtract) / avgExpPerGame;
+    }
+
+    public static double calculateGamesPerDay() {
+        return (((double) expLeft - expToSubtract) / avgExpPerGame) / daysSpan;
+    }
+
+    public static double calculateDailyTimeRequirement() {
+        return (calculateGamesPerDay() * avgGameTime) / 60.0d;
+    }
+
+    public static double calculatePercentage() {
+        return ((double) currentExpEarned / EXP_CONST) * 100.0d;
+    }
+
+    private static void out(String s) {
+        System.out.println(s);
+    }
+
+    private static void newLine() {
+        out(System.lineSeparator());
+    }
+
+    private static String format(double number) {
+        String formatted = new DecimalFormat("#.#")
+                .format(number);
+        return NumberFormat.getInstance().format(Double.parseDouble(formatted));
+    }
+}
